f01d3f8aff0bd41753cdca8e3c36b9691338236a
root <root@Rainbow.localdomain>
2019-11-18 11:31:20 +0100

    Remove avatar img
    On small video thumbnails removes avatar image for reading the title better

--- background.js
+++ background.js
@@ -1,7 +1,22 @@
-function calculate_windoes_on_row(row_witdh, total_videos) {
+function calculate_windoes_on_row(row_witdh, total_videos){
     let screen_width = screen.width - 305;
     let video_width = screen_width / total_videos;
+    console.log(video_width);
+    if(video_width <= 190){
+        remove_avatar_img('none');
+    } else {
+        remove_avatar_img('');
+    }
     return Math.round(row_witdh / video_width);
+}
+
+function remove_avatar_img(style){
+    let avatar_img = document.getElementsByClassName('yt-simple-endpoint');
+    for(let i = 0; i < avatar_img.length; i++){
+        if(avatar_img[i].id === 'avatar-link'){
+            avatar_img[i].style.display = style
+        }
+    }
 }
 
 function set_videos_on_row(total_videos){
@@ -9,8 +24,7 @@ function set_videos_on_row(total_videos){
     for(var i = 0; i < video_rows.length; i++){
         if(video_rows[i].style.cssText.includes('--ytd-rich-grid-items-per-row')){
             let row = calculate_windoes_on_row(video_rows[i].clientWidth, total_videos);
-            let val = `--ytd-rich-grid-items-per-row: ${row};--ytd-rich-grid-posts-per-row: 4; --ytd-rich-grid-movies-per-row: 9;`;
-            video_rows[i].style.cssText = val;
+            video_rows[i].style.cssText = `--ytd-rich-grid-items-per-row: ${row};--ytd-rich-grid-posts-per-row: 4; --ytd-rich-grid-movies-per-row: 9;`;
         };
     }
 }
